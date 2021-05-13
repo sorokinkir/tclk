ed770b6ec6a41f99162e21ba6bca39efdd80f982
Sorokin Kirill <raelf@mail.ru>
2021-05-13 20:24:38 +0700

    Косметические правки

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings.Web;
+
 
 namespace tclk.Controllers {
     public class HomeController : Controller {
-        //
         // Get: "/"
         public IActionResult Index() {
             return View();
         }
-        //
         // Get: "/cams/"
         public IActionResult Cams() {
             return View();
