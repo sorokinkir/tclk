66dab3e0c9219eaddf8f25e4f6e829236fb6fcea
Sorokin Kirill <raelf@mail.ru>
2021-05-18 21:20:38 +0700

    Файлы доступа к бд

--- Data/TclkContext.cs
+++ Data/TclkContext.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using tclk.Models;
+
 namespace tclk.Data
 {
-    public class TclkContext
+    public class TclkContext : DbContext
     {
-        
+        public TclkContext (DbContextOptions<TclkContext> options)
+            : base(options) { }
+        public DbSet<News> News { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -8,21 +8,46 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using tclk.Data;
 
 namespace tclk
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration) {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env) {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            
+            services.AddDbContext<TclkContext>(options => {
+                var connectionString = Configuration.GetConnectionString("TclkContext");
+                if (Environment.IsDevelopment())
+                {
+                    options.UseNpgsql(connectionString);
+                }
+                else if (Environment.IsProduction())
+                {
+                    options.UseNpgsql(connectionString);
+                }
+                else
+                {
+                    options.UseNpgsql(connectionString);
+                }
+            });
+
+            // services.AddDbContext<TclkContext>(options =>
+            //     options.UseNpgsql(Configuration.GetConnectionString("TclkContext"))
+            // );
+
             services.AddRouting(options => {
                 options.LowercaseUrls = true;
                 options.AppendTrailingSlash = true;
